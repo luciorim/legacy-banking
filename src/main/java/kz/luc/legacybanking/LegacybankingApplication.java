2efe70c1d814e9d5976f19daee1775aab3d17548
luciorim <88190686+luciorim@users.noreply.github.com>
2025-02-26 13:02:35 +0500

    initial

---
+++ src/main/java/kz/luc/legacybanking/LegacybankingApplication.java
@@ -0,0 +1,13 @@
+package kz.luc.legacybanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacybankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegacybankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/luc/legacybanking/LegacybankingApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.luc.legacybanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacybankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
